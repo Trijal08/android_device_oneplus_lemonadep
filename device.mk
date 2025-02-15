5e507332709d6b6c53aeaf67ce75ea280a9e5ae4
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-02-15 17:14:41 -0500

    Include extras such as BCR and ViPER4AndroidFX

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Include some extras if they exist
+-include $(LOCAL_PATH)/extras.mk
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxxhdpi
