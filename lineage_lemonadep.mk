c41bfc1ec62ee820fe9fde5382f2ecec6790dd42
Trijal Saha <97483939+Trijal08@users.noreply.github.com>
2025-04-02 17:40:03 -0400

    Partial bring-up for almost all custom ROMs

--- lineage_lemonadep.mk
+++ lineage_lemonadep.mk
@@ -10,17 +10,38 @@ TARGET_SUPPORTS_OMX_SERVICE := false
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from lemonadep device
-$(call inherit-product, device/oneplus/lemonadep/device.mk)
+DEVICE_CODENAME := lemonadep
+DEVICE_PATH := device/oneplus/lemonadep
+COMMON_PATH := device/oneplus/sm8350-common
+VENDOR_PATH := vendor/oneplus/lemonadep
+COMMON_VENDOR_PATH := vendor/oneplus/sm8350-common
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common stuff
+ROM_VENDOR := lineage
+ifdef ROM_VENDOR
+$(call inherit-product, vendor/$(ROM_VENDOR)/config/common_full_phone.mk)
+else
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+endif
 
-PRODUCT_NAME := lineage_lemonadep
-PRODUCT_DEVICE := lemonadep
+# Inherit device configuration
+ifdef ROM_VENDOR
+PRODUCT_NAME := $(ROM_VENDOR)_$(DEVICE_CODENAME)
+else
+PRODUCT_NAME := lineage_$(DEVICE_CODENAME)
+endif
+PRODUCT_DEVICE := $(DEVICE_CODENAME)
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := LE2125
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Build props
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
